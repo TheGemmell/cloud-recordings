6e7c8ad21c47378c53ca5f0704145c42af90bf71
Chris <fablekey@gmail.com>
2022-09-23 03:56:56 +1000

    weekNo function added

--- src/lib.ts
+++ src/lib.ts
@@ -1,11 +1,18 @@
+const startDate = new Date(2022, 8, 5);
+
 function convertDate(dateStr: string): Date {
   const date: string[] | Date = dateStr.split("/");
   [ date[0], date[1] ] = [ date[1], date[0] ];
   return new Date(date.join("/"));
 }
 
-function convertWeekNo(dateStr: Date) {
-
+function convertWeekNo(date: Date) {
+  const numOfDays = Math.floor((date - startDate) / (24 * 60 * 60 * 1000));
+  const weekNo = Math.ceil((numOfDays) / 7);
+  return weekNo;
 }
 
-export { convertDate }
+export { convertDate };
+
+
+
