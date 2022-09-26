0d5e71d9cadd0ff15f59de429670571305e47d0d
Chris <fablekey@gmail.com>
2022-09-26 12:30:50 +1000

    implemented appShell and got Grid displaying

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import { AppShell, Header, Navbar } from '@mantine/core'
+import { AppShell } from '@mantine/core'
+import { LessonWeeks, prepareLinks } from './lib';
 import LessonNav from './LessonNav';
+import LessonGrid from './LessonGrid';
 
 type SheetResponse = {
   values: string[][];
@@ -10,20 +12,22 @@ type SheetResponse = {
 }
 
 function App() {
-  const [ lessons, setLessons ] = useState<string[][]>();
-  const [activeWeek, setActiveWeek] = useState('Releases');
-  const [activeDay, setActiveDay] = useState('Settings');
+  const [ lessons, setLessons ] = useState<LessonWeeks>();
+  const [ activeWeek, setActiveWeek ] = useState(1);
+  const [ activeDay, setActiveDay ] = useState(1);
 
   console.log(lessons)
   console.log(activeDay, activeWeek)
+
   useEffect(() => {
     async function getLinks(range: string) {
       let request = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/1ZfLVzYrXJcAARFfLzuYSEI1ylvT16CWeRwY4QFdf1fU/values/${range}?key=AIzaSyCJv-Xb0xp1n_qFvzf0dtqTp9dKtHdS7N4`)
       let data: SheetResponse = await request.json();
-      setLessons(data.values);
+      setLessons(prepareLinks(data.values));
     }
     getLinks("A2:C800")
   },[])
+
 
   return (
     <>
@@ -31,7 +35,7 @@ function App() {
       <AppShell
       navbar={<LessonNav activeDay={activeDay} activeWeek={activeWeek} setActiveWeek={setActiveWeek} setActiveDay={setActiveDay}/>}
       >
-        Hello
+        <LessonGrid data={lessons} filterDay={activeDay} filterWeek={activeWeek} />
       </AppShell>
       :
       <h1>Loading...</h1>
